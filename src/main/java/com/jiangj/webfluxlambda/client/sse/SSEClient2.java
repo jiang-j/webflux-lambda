ffd7523957c30c8eba3521ee4d3b41a063384927
jiangj <jiangjian318@gmail.com>
2018-07-20 18:52:09 +0800

    update

---
+++ src/main/java/com/jiangj/webfluxlambda/client/sse/SSEClient2.java
@@ -0,0 +1,56 @@
+package com.jiangj.webfluxlambda.client.sse;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.jiangj.webfluxlambda.ExecutorConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.reactive.function.BodyExtractors;
+import org.springframework.web.reactive.function.client.WebClient;
+import sun.nio.ch.ThreadPool;
+
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * @author Jiang Jian
+ * @since 2018/7/10
+ */
+public class SSEClient2 {
+
+    public static void main(String[] args) {
+        SSEClient2 client2 = new SSEClient2();
+        client2.test();
+    }
+
+
+    public void test() {
+
+        ThreadFactory nameThreadFactory = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build();
+
+        ExecutorService pool = new ThreadPoolExecutor(5, 200,
+                0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(1024), nameThreadFactory,
+                new ThreadPoolExecutor.AbortPolicy());
+
+
+        for (int i = 0; i < 1000; i--) {
+            pool.execute(() -> {
+                WebClient client = WebClient.create("http://127.0.0.1:8082/queryByRecordId?recordId=" + new Random().nextLong());
+                client.get()
+                        .uri("")
+                        .accept(MediaType.TEXT_EVENT_STREAM)
+                        .exchange()
+                        .flatMapMany(clientResponse -> clientResponse.body(BodyExtractors.toFlux(new ParameterizedTypeReference<ServerSentEvent<String>>() {
+                        })))
+                        .filter(sse -> Objects.nonNull(sse.data()))
+                        .map(ServerSentEvent::data)
+                        .doOnNext(System.out::println)
+                        .blockFirst();
+
+            });
+        }
+    }
+}
--- src/test/java/com/jiangj/webfluxlambda/WebfluxLambdaApplicationTests.java
+++ src/test/java/com/jiangj/webfluxlambda/WebfluxLambdaApplicationTests.java
@@ -2,15 +2,30 @@ package com.jiangj.webfluxlambda;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.BodyExtractors;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Objects;
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebfluxLambdaApplicationTests {
 
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
+
     @Test
-    public void contextLoads() {
+    public void test(){
+
     }
+
 
 }
